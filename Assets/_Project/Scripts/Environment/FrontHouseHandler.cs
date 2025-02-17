3c9af76b7de52ed91668a1236e7d992e78266e0e
hanoirocker <Rafael.Aguero@hearst.com>
2025-02-17 18:06:51 -0300

    12G: Fernandez suicide first files

--- Assets/_Project/Scripts/Environment/FrontHouseHandler.cs
+++ Assets/_Project/Scripts/Environment/FrontHouseHandler.cs
@@ -4,8 +4,13 @@ namespace TwelveG.Environment
 
     public class FrontHouseHandler : MonoBehaviour
     {
+        [Header("Objects references")]
+        [SerializeField] private GameObject fernandezDeadPrefab;
         [SerializeField] private Light livingRoomLight;
         [SerializeField] private Light entranceLight;
+
+        [Header("Audio references")]
+        [SerializeField] private AudioSource shotGunAudioSource;
 
 
         public void SwitchLivingRoomLight(bool turnOnLight)
@@ -16,6 +21,12 @@ namespace TwelveG.Environment
         public void SwitchEntranceLight(bool turnOnLight)
         {
             entranceLight.enabled = turnOnLight;
+        }
+
+        public void TriggerFernandezSuicide()
+        {
+            fernandezDeadPrefab.SetActive(true);
+            shotGunAudioSource.Play();
         }
     }
 }
