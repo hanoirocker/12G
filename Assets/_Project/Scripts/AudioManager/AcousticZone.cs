d6c5a22f74969318e9c8772c89ff55c08aae15f2
hanoirocker <Rafael.Aguero@hearst.com>
2025-07-20 14:21:54 -0300

    feat: create acoustic zones and mod EnvironmentAudioController

---
+++ Assets/_Project/Scripts/AudioManager/AcousticZone.cs
@@ -0,0 +1,28 @@
+
+namespace TwelveG.AudioManager
+{
+  using UnityEngine;
+
+  public class AcousticZone : MonoBehaviour
+  {
+    [SerializeField] private Transform audioZoneTransform;
+
+    private void OnTriggerEnter(Collider other)
+    {
+      if (other.CompareTag("Player"))
+      {
+        Debug.Log($"Entered zone {gameObject.name}");
+        EnvironmentAudioController.Instance.EnteredAcousticZone(audioZoneTransform);
+      }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+      if (other.CompareTag("Player"))
+      {
+        Debug.Log($"Exited zone {gameObject.name}");
+        EnvironmentAudioController.Instance.ExitedAcousticZone(audioZoneTransform);
+      }
+    }
+  }
+}
