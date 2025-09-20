45822563324c53f1f7e29bc4f0dc184069d503fd
hanoirocker <Rafael.Aguero@hearst.com>
2025-09-19 21:44:17 -0300

    feat: work on Fernandez prefabs and mods on materials

---
+++ Assets/_Project/Scripts/EnvironmentController/RandomFlickerLight.cs
@@ -0,0 +1,24 @@
+namespace TwelveG.EnvironmentController
+{
+  using UnityEngine;
+
+  public class RandomFlickerLight : MonoBehaviour
+  {
+    public float minIntensity = 0.5f;
+    public float maxIntensity = 2f;
+    public float flickerSpeed = 0.05f;
+
+    private Light _light;
+
+    private void Awake()
+    {
+      _light = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+      float noise = Mathf.PerlinNoise(Time.time * 10f, 0f);
+      _light.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+    }
+  }
+}
