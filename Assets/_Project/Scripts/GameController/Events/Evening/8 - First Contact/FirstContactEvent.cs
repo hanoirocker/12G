9d0c550ac47775a9bf45310b5dfed57e8708c062
hanoirocker <103009788+hanoirocker@users.noreply.github.com>
2025-10-02 00:32:03 -0300

    feat: create next First Contact event and coroutine event script

---
+++ Assets/_Project/Scripts/GameController/Events/Evening/8 - First Contact/FirstContactEvent.cs
@@ -0,0 +1,54 @@
+namespace TwelveG.GameController
+{
+    using System.Collections;
+    using UnityEngine;
+
+    public class FirstContactEvent : GameEventBase
+    {
+        [Header("References")]
+        // [SerializeField] private GameObject suicideTriggerColliders;
+
+        [Header("Event options")]
+        [SerializeField, Range(1, 10)] private int initialTime = 1;
+        // public Transform suicideViewTransform;
+
+        [Header("Text event SO")]
+        //[SerializeField] private List<ObservationTextSO> eventObservationsTextsSOs;
+        //[SerializeField] private ObservationTextSO mainDoorsFallbacksTextsSO;
+        //[SerializeField] private GameEventSO updateFallbackTexts;
+
+        [Header("EventsSO references")]
+        // [SerializeField] private GameEventSO onObservationCanvasShowText;
+
+        [Header("Other eventsSO references")]
+        // [SerializeField] private GameEventSO drawerCanBeInteracted;
+
+        private bool allowNextAction = false;
+
+        public override IEnumerator Execute()
+        {
+            print("<------ FIRSTCONTACT EVENT NOW -------->");
+
+            yield return new WaitForSeconds(initialTime);
+
+            // updateFallbackTexts.Raise(this, mainDoorsFallbacksTextsSO);
+
+            yield return new WaitUntil(() => allowNextAction);
+            ResetAllowNextActions();
+        }
+        public void AllowNextActions(Component sender, object data)
+        {
+            allowNextAction = true;
+        }
+
+        public void ResetAllowNextActions()
+        {
+            allowNextAction = false;
+        }
+
+        public void StopEvent()
+        {
+            StopAllCoroutines();
+        }
+    }
+}
