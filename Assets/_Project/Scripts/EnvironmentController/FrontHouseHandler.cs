91ba3352a6bd6f27fb5883c90d8c13ec6dc27ee2
hanoirocker <103009788+hanoirocker@users.noreply.github.com>
2025-11-11 02:54:00 -0300

    feat: turn off front house tv after an amount of time

--- Assets/_Project/Scripts/EnvironmentController/FrontHouseHandler.cs
+++ Assets/_Project/Scripts/EnvironmentController/FrontHouseHandler.cs
@@ -1,6 +1,8 @@
 namespace TwelveG.EnvironmentController
 {
+    using System.Collections;
     using UnityEngine;
+    using UnityEngine.Video;
 
     public class FrontHouseHandler : MonoBehaviour
     {
@@ -9,6 +11,9 @@ namespace TwelveG.EnvironmentController
         [SerializeField] private Transform chairNoBlood;
         [SerializeField] private Transform chairWithBlood;
         [SerializeField] private Light entranceLight;
+
+        [SerializeField, Range(10f, 1000f)] private float timeBeforeTVTurnedOff = 180f;
+        [SerializeField] private GameObject tvObject;
 
 
         public void SwitchEntranceLight(bool turnOnLight)
@@ -21,6 +26,14 @@ namespace TwelveG.EnvironmentController
             chairNoBlood.gameObject.SetActive(false);
             chairWithBlood.gameObject.SetActive(true);
             Instantiate(fernandezDeadPrefab, chairWithBlood);
+            StartCoroutine(TurnOffTVAfterDelay(timeBeforeTVTurnedOff));
+        }
+
+        private IEnumerator TurnOffTVAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+            tvObject.GetComponentInChildren<VideoPlayer>().gameObject.SetActive(false);
+            tvObject.GetComponentInChildren<Light>().gameObject.SetActive(false);
         }
     }
 }
