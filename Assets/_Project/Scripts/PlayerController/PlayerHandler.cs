a7d40715875dba6d3432c95809bb15648ff38f83
hanoirocker <Rafael.Aguero@hearst.com>
2025-12-26 13:10:59 -0300

    refactor: playerHandler commands

--- Assets/_Project/Scripts/PlayerController/PlayerHandler.cs
+++ Assets/_Project/Scripts/PlayerController/PlayerHandler.cs
@@ -46,79 +46,69 @@ namespace TwelveG.PlayerController
             switch (data)
             {
                 case ToggleToObjectExamination cmd:
-                    playerInteraction.enabled = !cmd.Enabled;
-                    playerContemplation.enabled = !cmd.Enabled;
-                    playerAddItem.enabled = !cmd.Enabled;
-                    cameraZoom.enabled = !cmd.Enabled;
-                    headBobController.enabled = !cmd.Enabled;
-                    playerShortcuts.playerCanOpenPauseMenu = !cmd.Enabled;
+                    bool isActive = !cmd.Enabled;
+                    
+                    ToggleInteractionModules(isActive);
+                    SwitchPlayerControllers(isActive);
+                    
+                    playerShortcuts.playerCanOpenPauseMenu = isActive;
                     HandleExamination(cmd.Enabled);
-                    SwitchPlayerControllers(!cmd.Enabled);
                     break;
+
                 case EnableControlCanvasAccess cmd:
                     playerShortcuts.playerCanOpenCanvasControls = cmd.Enabled;
                     break;
+
                 case EnablePauseMenuCanvasAccess cmd:
                     playerShortcuts.playerCanOpenPauseMenu = cmd.Enabled;
                     break;
+
                 case EnablePlayerSprint cmd:
                     fPController.EnableSprint(cmd.Enabled);
                     break;
+
                 case TogglePlayerCapsule cmd:
                     playerCapsule.SetActive(cmd.Enabled);
                     break;
+
                 case TogglePlayerMainCamera cmd:
                     mainCamera.SetActive(cmd.Enabled);
                     break;
+
                 case EnablePlayerShortcuts cmd:
                     playerShortcuts.enabled = cmd.Enabled;
                     break;
+
                 case EnablePlayerHeadLookAround cmd:
                     headLookAround.enabled = cmd.Enabled;
                     break;
+
                 case EnablePlayerControllers cmd:
                     SwitchPlayerControllers(cmd.Enabled);
                     break;
+
                 case EnablePlayerCameraZoom cmd:
                     cameraZoom.enabled = cmd.Enabled;
                     break;
+
                 default:
                     Debug.LogWarning($"[PlayerHandler] Comando desconocido recibido: {data}");
                     break;
             }
         }
 
-        private void HandleExamination(bool isExamining)
-        {
-            if (playerInventory.PlayerIsUsingFlashlight())
-            {
-                mainCamera.GetComponent<Light>().enabled = isExamining;
-            }
-
-            playerInventory.HandleExaminationWhileUsingItems(isExamining);
-        }
-
-        private void SwitchPlayerControllers(bool enabled)
-        {
-            fPController.enabled = enabled;
-            playerAudioSource.enabled = enabled;
-            playerSoundsHandler.enabled = enabled;
-            playerInput.enabled = enabled;
-            characterController.enabled = enabled;
-            headBobController.enabled = enabled;
-        }
-
         public void PlayerDyingMode()
         {
+            // Cancelar cualquier examen en curso si existe
             mainCamera.GetComponentInChildren<ExaminableObject>()?.CancelExaminationMode();
+
+            // Desactivar accesos directos
             playerShortcuts.enabled = false;
             playerShortcuts.playerCanOpenPauseMenu = false;
+
+            // Apagar todos los sistemas de control e interacción
             SwitchPlayerControllers(false);
-            playerInteraction.enabled = false;
-            playerContemplation.enabled = false;
-            playerAddItem.enabled = false;
-            cameraZoom.enabled = false;
-            headBobController.enabled = false;
+            ToggleInteractionModules(false);
         }
 
         public void PauseGame(Component sender, object data)
@@ -128,6 +118,37 @@ namespace TwelveG.PlayerController
             playerCapsule.SetActive(enable);
             playerContemplation.enabled = enable;
             playerInteraction.enabled = enable;
+        }
+
+        private void HandleExamination(bool isExamining)
+        {
+            if (playerInventory.PlayerIsUsingFlashlight())
+            {
+                var camLight = mainCamera.GetComponent<Light>();
+                if (camLight != null) camLight.enabled = isExamining;
+            }
+
+            playerInventory.HandleExaminationWhileUsingItems(isExamining);
+        }
+
+        // Agrupa todos los scripts de interacción con el entorno
+        private void ToggleInteractionModules(bool enabled)
+        {
+            playerInteraction.enabled = enabled;
+            playerContemplation.enabled = enabled;
+            playerAddItem.enabled = enabled;
+            cameraZoom.enabled = enabled;
+        }
+
+        // Agrupa todos los scripts de movimiento y input físico
+        private void SwitchPlayerControllers(bool enabled)
+        {
+            fPController.enabled = enabled;
+            playerAudioSource.enabled = enabled;
+            playerSoundsHandler.enabled = enabled;
+            playerInput.enabled = enabled;
+            characterController.enabled = enabled;
+            headBobController.enabled = enabled;
         }
     }
 }
